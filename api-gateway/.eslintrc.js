2ff24541b8ffe24f54c69452cd98a106119539e0
William Ducta <wducta@yondu.com>
2022-03-29 12:51:59 +0800

    CHORE: Return to default eslint settings

--- api-gateway/.eslintrc.js
+++ api-gateway/.eslintrc.js
@@ -1,37 +1,30 @@
 module.exports = {
-  // parser: '@typescript-eslint/parser',
-  // parserOptions: {
-  //   project: 'tsconfig.json',
-  //   sourceType: 'module',
-  // },
-  plugins: [
-    // '@typescript-eslint/eslint-plugin',
-    'prettier',
-  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    // 'plugin:@typescript-eslint/recommended',
-    // 'plugin:prettier/recommended',
-    'airbnb',
-    'prettier',
-    'plugin:node/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
   root: true,
   env: {
     node: true,
     jest: true,
   },
-  // ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js'],
   rules: {
-    'prettier/prettier': 'error',
-    'no-unused-vars': 'warn',
-    'no-console': 'off',
-    'func-names': 'off',
-    'no-process-exit': 'off',
-    'object-shorthand': 'off',
-    'class-methods-use-this': 'off',
-    // '@typescript-eslint/interface-name-prefix': 'off',
-    // '@typescript-eslint/explicit-function-return-type': 'off',
-    // '@typescript-eslint/explicit-module-boundary-types': 'off',
-    // '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
